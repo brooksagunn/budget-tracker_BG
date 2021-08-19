69be2c1d09f796230b2e60caccd10f95d8bf574a
Brooks Gunn <brooks.a.gunn@gmail.com>
2021-08-19 13:46:45 -0400

    Added onungradeneeded method to db that maintains the latest versionof the db

--- public/db.js
+++ public/db.js
@@ -1,2 +1,15 @@
 const db;
 const budgetVersion;
+
+const req = indexedDB.open('BudgetDB', budgetVersion || 21);
+
+req.onupgradeneeded = (e) => {
+    const { oldVersion } = e;
+    const newVersion = e.newVersion || db.newVersion;
+
+    console.log(`DB version replaced: ${oldVersion} -> ${newVersion}`);
+
+    db = e.target.result;
+
+    if (db.objectStoreNames) db.createObjectStore('BudgetStore', { autoIncrement : true });
+}
